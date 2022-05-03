c4f4bf78c883d0a157dbcad074c208585f658f48
Tonychen0227 <tony.chen@outlook.com>
2022-05-02 20:25:27 -0700

    first commit

---
+++ api.py
@@ -0,0 +1,103 @@
+import json
+
+from graphqlclient import GraphQLClient
+
+SMASH_ULTIMATE_GAME_ID = 1386
+
+SEEDING_TO_ROUNDS_FROM_FINAL = [1, 2, 3, 4, 5, 7, 9, 13, 17, 25, 33, 49, 65, 97, 129, 193, 257, 385, 513, 769, 1025, 1537, 2049, 3073, 4097]
+
+
+class API:
+    def __init__(self, token):
+        self.client = GraphQLClient('https://api.smash.gg/gql/alpha')
+        self.client.inject_token(f'Bearer {token}')
+
+    def get_effective_seed(self, seed):
+        prev_value = -1
+        for index, value in enumerate(SEEDING_TO_ROUNDS_FROM_FINAL):
+            if value == seed:
+                return value
+            if value >= seed:
+                return prev_value
+
+            prev_value = value
+
+    def get_round_from_seed(self, seed):
+        prev_index = -1
+        for index, value in enumerate(SEEDING_TO_ROUNDS_FROM_FINAL):
+            if value == seed:
+                return index
+            if value >= seed:
+                return prev_index
+
+            prev_index = index
+
+    def get_seed_performance(self, expected, actual):
+        expected_finish_round = self.get_round_from_seed(expected)
+        actual_finish_round = self.get_round_from_seed(actual)
+
+        return expected_finish_round - actual_finish_round
+
+    def get_tournament_ultimate_standings(self, slug):
+        r = self.client.execute('''
+            query GetTournament($slug: String, $videogameId: [ID]) {
+              tournament(slug: $slug) {
+                name
+                events(filter: {videogameId: $videogameId}) {
+                    name
+                    standings(query: {perPage: 256}) {
+                        nodes {
+                            entrant {
+                                seeds {
+                                    seedNum
+                                    placement
+                                }
+                                name
+                            }
+                            placement
+                        }
+                    }
+                }
+              }
+            }
+            ''',
+            {
+                "slug": slug,
+                "videogameId": [SMASH_ULTIMATE_GAME_ID]
+            })
+
+        tournament_results = json.loads(r)["data"]["tournament"]
+
+        custom_data = {
+            "tournament_name": tournament_results["name"],
+            "events": []
+        }
+
+        for event in tournament_results["events"]:
+            event_json = {
+                "event_name": event["name"],
+                "standings": []
+            }
+
+            for standing in event["standings"]["nodes"]:
+                if len(standing["entrant"]["seeds"]) > 1:
+                    print(f"More than one seed for {standing}")
+
+                result = {
+                    "name": standing["entrant"]["name"],
+                    "seed": standing["entrant"]["seeds"][0]["seedNum"],
+                    "placement": standing["placement"]
+                }
+
+                result["performance"] = self.get_seed_performance(result["seed"], result["placement"])
+                result["effectiveSeed"] = self.get_effective_seed(result["seed"])
+
+                event_json["standings"].append({
+                    "name": result["name"],
+                    "performance": result["performance"],
+                    "placement": result["placement"]
+                })
+
+            custom_data["events"].append(event_json)
+
+        return custom_data
---
+++ main.py
@@ -0,0 +1,79 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+from api import API
+
+
+def plot_tournament(api_key, tournament_slug):
+    smashgg_api = API(api_key)
+    results = smashgg_api.get_tournament_ultimate_standings(tournament_slug)
+
+    for event in results["events"]:
+        if "Singles" not in event["event_name"]:
+            continue
+        y = []
+        x = []
+        n = []
+
+        for standing in event["standings"]:
+            y.append(standing["performance"])
+            x.append(standing["placement"])
+            n.append(standing["name"])
+
+        existing = {}
+
+        for index in range(0, len(y)):
+            cur_x = x[index]
+            cur_y = y[index]
+            cur_n = n[index]
+
+            if cur_x not in existing:
+                existing[cur_x] = {}
+
+            if cur_y not in existing[cur_x]:
+                existing[cur_x][cur_y] = []
+
+            existing[cur_x][cur_y].append(cur_n)
+
+        final_y = []
+        final_x = []
+        final_n = []
+
+        for key in existing:
+            for temp_y in existing[key]:
+                final_x.append(key)
+                final_y.append(temp_y)
+                final_n.append(existing[key][temp_y])
+
+        fig, ax = plt.subplots()
+
+        plt.title(f"{results['tournament_name']} - {event['event_name']}")
+        plt.yticks([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
+        plt.xlabel("placement")
+        plt.ylabel("seed performance")
+
+        ax.scatter(np.array(final_x).astype('str'), final_y)
+        for i, txt in enumerate(final_n):
+            if len(txt) < 3:
+                caption = ','.join(txt)
+            else:
+                caption = f"{len(txt)} players"
+
+            ax.annotate(caption, (str(final_x[i]), final_y[i]))
+
+        ax.set_xlabel("placement")
+        ax.set_ylabel("seed performance")
+
+
+if __name__ == '__main__':
+    token = os.environ["SMASHGG_TOKEN"]
+    raw_slug = "https://smash.gg/tournament/grandslam-3/event/ultimate-singles/brackets/1084049/1693277"
+    slug = raw_slug.split('/')[4]
+    for slug in [
+        "grandslam-3",
+        "vancouver-battle-royale-resurrection",
+        "grandslam-4"
+    ]:
+        plot_tournament(token, slug)
+    plt.show()
